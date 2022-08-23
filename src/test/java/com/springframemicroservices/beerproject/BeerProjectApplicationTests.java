5d3a013949ce1cbe93be9240a70781c25ef563de
anna rozou <anna.rozou@vodafoneinnovus.com>
2022-08-23 16:45:48 +0300

    Initial commit

---
+++ src/test/java/com/springframemicroservices/beerproject/BeerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframemicroservices.beerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
